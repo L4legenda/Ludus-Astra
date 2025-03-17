e4580230d027f5d749f6c3edb70ea66115ac4474
l4legenda <l4legenda@gmail.com>
2025-03-18 01:22:47 +0500

    migrations

---
+++ ToDo_LudusAstra/Migrations/20250312045315_exp task.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDo_LudusAstra.Migrations
+{
+    /// <inheritdoc />
+    public partial class exptask : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "exp",
+                table: "Tasks",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "exp",
+                table: "Tasks");
+        }
+    }
+}
